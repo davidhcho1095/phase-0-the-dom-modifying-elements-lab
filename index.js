33a5fa479663e1341898e76900291f3ff42edba4
David Cho <davidhcho1095@gmail.com>
2022-04-14 15:56:52 -0400

    commit tree 47ff85cd42cd2f49568b9b680196c7726ee2bf2a

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// Write your code here!
+const mainElement = document.querySelector("#main").remove();
+
+const newHeader=document.createElement('h1');
+newHeader.id = "victory";
+newHeader.setAttribute = "h1#victory";
+newHeader.innerHTML = "David is the champion"
